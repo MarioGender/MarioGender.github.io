e29d423cbf9cd56778731fb4e7afce8bfcc467ce
Mario Gender <Gender.Mario@outlook.com>
2019-05-05 19:40:47 +0200

    JS and Finish Touches
    Page 1 complete

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,6 @@
 var btn = $('#topbtn');
 var brokenLinks = $('.brokenlink');
 
-$(window).resize(function() {
-
 /*
 var x = document.getElementById("header").offsetWidth;
 nav.offsetWidth = x;
@@ -10,12 +8,13 @@ console.log(header.offsetWidth + '+' + nav.offsetWidth);
 */
     
 /////////////////RESIZE FUNCTION/////////////////  
+$(window).resize(function() {
 if (window.innerWidth < 500) {
     CV.innerHTML = 'CV';
 }
 else {
     CV.innerHTML = 'Curriculum Vitae';
-}
+}  
 
 if ($(window).scrollTop() > 300 && window.innerWidth < 580) {
     btn.addClass('show');
@@ -43,4 +42,16 @@ var brokenLinks = $('.brokenlink');
 brokenLinks.on('click', function(e) {
       event.preventDefault();
       alert('Link has nothing to lead to, YET!');         
+});
+
+/////////////////HOVER NAV FUNCTION/////////////////
+var navitem = $('.nav-item').not('#cvnav').not('.current');
+var current = $('.nav-item.current');
+
+navitem.mouseenter(function() {
+   current.removeClass("current");        
+});
+
+navitem.mouseleave(function() {
+    current.addClass("current");
 });
